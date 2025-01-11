b7dcb1526ab2814af35c88ce5e92607e4009b419
Shaigro <robin.boucher@epita.fr>
2025-01-11 05:29:35 +0100

    add save file binding
    'f' then 's' to save the current map as nabe.json

--- src/EditorContext.tsx
+++ src/EditorContext.tsx
@@ -1,6 +1,12 @@
 import { createContext, ReactNode, useContext, useState } from 'react'
 
-import { useHotkeys } from '@mantine/hooks'
+import { Modal } from '@mantine/core'
+import { getHotkeyHandler, useDisclosure, useHotkeys } from '@mantine/hooks'
+import { notifications } from '@mantine/notifications'
+
+import { Binding } from './components/Binding'
+import { writeFile } from './tauri/file'
+import { useMapContext } from './MapContext'
 
 type RootBinding = 'file'
 
@@ -15,13 +21,51 @@ export const EditorContextProvider = ({
 }: {
   children: ReactNode
 }) => {
+  const { blocks } = useMapContext()
+  const [opened, { open, close }] = useDisclosure(false)
+
   const [pressedRootBinding, setPressedRootBinding] =
     useState<EditorInformation['pressedRootBinding']>(null)
 
-  useHotkeys([['F', () => alert('hello')]])
+  useHotkeys([['F', open]])
 
   return (
     <EditorContext.Provider value={{ pressedRootBinding }}>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="File options"
+        centered
+        onKeyDown={getHotkeyHandler([
+          [
+            'S',
+            async (e) => {
+              e.stopPropagation()
+              e.preventDefault()
+
+              const { failed, filepath } = await writeFile(
+                JSON.stringify(blocks),
+                'nabe.json',
+              )
+
+              if (!failed) {
+                notifications.show({
+                  title: 'File saved',
+                  message: `Saved at ${filepath}nabe.json`,
+                })
+              } else {
+                notifications.show({
+                  color: 'red',
+                  title: 'Failed to save file',
+                  message: `Failed to save at ${filepath}`,
+                })
+              }
+            },
+          ],
+        ])}
+      >
+        <Binding binding="S" desc="Save file" />
+      </Modal>
       {children}
     </EditorContext.Provider>
   )
