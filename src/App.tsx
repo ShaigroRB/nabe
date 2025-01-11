63a691e36c98cceb099f6a3f30c325aca88c763d
Shaigro <robin.boucher@epita.fr>
2025-01-11 02:26:40 +0100

    catch & display errors thrown by the editor (pixiJS)

--- src/App.tsx
+++ src/App.tsx
@@ -26,43 +26,9 @@ export default App
  * Responsive shell with header, collapsable navbar and content.
  */
 function CollapseDesktop() {
-  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true)
-
   return (
-    <AppShell
-      header={{ height: 60 }}
-      aside={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { desktop: !desktopOpened },
-      }}
-      padding={0}
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger
-            opened={desktopOpened}
-            onClick={toggleDesktop}
-            visibleFrom="sm"
-            size="sm"
-          />
-          <PlaceholderLogo size={30} />
-        </Group>
-      </AppShell.Header>
-      <AppShell.Aside p="md">Aside</AppShell.Aside>
-      <AppShell.Main
-        style={{
-          marginLeft: '4rem',
-          padding: 0,
-          paddingTop: 60,
-          height: '100%',
-          border: '1px red dotted',
-        }}
-      >
-        <ErrorBoundary fallback={<div>Editor crashed</div>}>
-          <Editor />
-        </ErrorBoundary>
-      </AppShell.Main>
-    </AppShell>
+    <ErrorBoundary fallback={<div>Editor crashed</div>}>
+      <Editor />
+    </ErrorBoundary>
   )
 }
