d97067271de8e0bb160405da7612b4a05d307c74
Shaigro <robin.boucher@epita.fr>
2025-01-11 02:26:41 +0100

    display preview of block to be drawn & handle placement of a block
    no info saved in the map context yet

--- src/canvas/Editor.tsx
+++ src/canvas/Editor.tsx
@@ -2,8 +2,14 @@ import { useEffect, useReducer } from 'react'
 import * as PIXI from 'pixi.js'
 
 import { DebugFunc, useDebugContext } from '../DebugContext'
+import { roundDownToNearestPositiveMultiple as roundDownToNearestPositiveMultiple } from '../utils/numbers'
 
-import { generateGrid } from './grid'
+import { CELL_SIZE, generateGrid } from './grid'
+import {
+  ZINDEX_CONTAINER_DETECT_EVENTS,
+  ZINDEX_LAYER_BLOCKS,
+  ZINDEX_PREVIEW_PLACED_OBJECT,
+} from './zIndexes'
 
 const PADDING_CANVAS = 16
 const windowHeight = window.innerHeight
@@ -39,7 +45,7 @@ const useInitializePixiMainContainer = () => {
     }
 
     waitForInitializationPixiApp().catch(console.error)
-  }, [editorContainer, setDebug])
+  }, [editorContainer])
 
   return editorContainer
 }
@@ -63,11 +69,13 @@ const initializePixiApp = async (
   const mainContainer = new PIXI.Container()
   mainContainer.sortableChildren = true
   mainContainer.renderable = true
-  mainContainer.x = PADDING_CANVAS
-  mainContainer.y = PADDING_CANVAS
 
   const grid = generateGrid(gridWidth, gridHeight)
+  grid.x = PADDING_CANVAS
+  grid.y = PADDING_CANVAS
   const blocks = blocksLayer(grid.width, grid.height, debug)
+  blocks.x = PADDING_CANVAS
+  blocks.y = PADDING_CANVAS
 
   mainContainer.addChild(grid)
   mainContainer.addChild(blocks)
@@ -75,21 +83,72 @@ const initializePixiApp = async (
   app.stage.addChild(mainContainer)
 }
 
-function blocksLayer(width: number, height: number, debug: DebugFunc) {
-  const layer = new PIXI.Container({ width, height, alpha: 0 })
+function getNearestLocalPosition(
+  evt: PIXI.FederatedPointerEvent,
+  container: PIXI.Container,
+) {
+  const localPosition = evt.getLocalPosition(container)
+  const nearestX = roundDownToNearestPositiveMultiple(
+    localPosition.x,
+    CELL_SIZE,
+  )
+  const nearestY = roundDownToNearestPositiveMultiple(
+    localPosition.y,
+    CELL_SIZE,
+  )
 
-  const graphicsToDetectEvents = new PIXI.Graphics({ width, height, alpha: 0 })
-  graphicsToDetectEvents.zIndex = 0
-  graphicsToDetectEvents.eventMode = 'static'
-  graphicsToDetectEvents.on('click', (e) => {
-    debug(`x: ${e.x}; y: ${e.y}`)
+  return { nearestX, nearestY }
+}
+
+function blocksLayer(width: number, height: number, debug: DebugFunc) {
+  const layer = new PIXI.Container({ width, height })
+  layer.sortableChildren = true
+
+  // where blocks are drawn
+  const blockGraphics = new PIXI.Graphics({ zIndex: ZINDEX_LAYER_BLOCKS })
+
+  // where block would be drawn if user clicks
+  const hoveredBlockGraphics = new PIXI.Graphics({
+    alpha: 0.3,
+    zIndex: ZINDEX_PREVIEW_PLACED_OBJECT,
   })
+  let prevHoveredPos = { x: -1, y: -1 }
+
+  // this graphics is simply there to detect where the blocks will be drawn
+  const events = new PIXI.Graphics({ width, height, alpha: 0 })
+  events.zIndex = ZINDEX_CONTAINER_DETECT_EVENTS
+  events.eventMode = 'static'
+
+  events.on('mousemove', (e) => {
+    const { nearestX, nearestY } = getNearestLocalPosition(e, layer)
+    // display a preview of the object to be placed (snapped to the grid)
+    if (nearestX === prevHoveredPos.x && nearestY === prevHoveredPos.y) {
+      return
+    } else {
+      hoveredBlockGraphics.clear()
+
+      hoveredBlockGraphics
+        .rect(nearestX, nearestY, CELL_SIZE, CELL_SIZE)
+        .fill(0xff0000)
+
+      prevHoveredPos = { x: nearestX, y: nearestY }
+    }
+  })
+
+  events.on('pointerdown', (e) => {
+    const { nearestX, nearestY } = getNearestLocalPosition(e, layer)
+    // draw a block to at the pointer position (snap it to the grid)
+    blockGraphics.rect(nearestX, nearestY, CELL_SIZE, CELL_SIZE).fill(0x000000)
+  })
+
+  layer.addChild(blockGraphics)
+  layer.addChild(hoveredBlockGraphics)
 
   // define a rectangle so something appears and can be clicked
   // even though it is transparent
-  graphicsToDetectEvents.rect(0, 0, width, height).fill(0xff00f0)
+  events.rect(0, 0, width, height).fill(0xff00f0)
 
-  layer.addChild(graphicsToDetectEvents)
+  layer.addChild(events)
 
   return layer
 }
