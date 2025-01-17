7e68b4316a301f6492f053459f38a3467c3e3ac8
Shaigro <robin.boucher@epita.fr>
2025-01-18 00:55:06 +0100

    update <Editor /> to use new structure elements & can change selected object type
    Use the new structure elements returned by `createPixiAppStructure()`.
    
    There is now 3 `useEffect` compared to one before:
    - 1) initialize the pixi app
    - 2) draw the map or redraw it if needed (e.g. map imported)
    - 3) set events listeners or reset them if needed (e.g. selected object
    changed)
    
    Redrawing the map & resetting the events listeners is way easier than
    before.
    Before, we used to remove all children from the root container, create
    new ones with the same settings and add them back to the root container.
    Now, we only clear the layer/container that is truly affected:
    - redraw only affects the drawing container
    - reset of events listeners only affects the events layer

--- src/canvas/Editor.tsx
+++ src/canvas/Editor.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import * as PIXI from 'pixi.js'
 
+import { EditorContextInformation, useEditorContext } from '../EditorContext'
 import { MapContextInformation, useMapContext } from '../MapContext'
-import { Block } from '../types'
+import { Block, MapInformation } from '../types'
 
 import { onMouseMove } from './events/mousemove'
 import { ObjProperties, onPointerDown } from './events/pointerdown'
-import { CELL_SIZE, generateGrid } from './grid'
+import { CELL_SIZE } from './grid'
 import {
-  ZINDEX_CONTAINER_DETECT_EVENTS,
-  ZINDEX_LAYER_BLOCKS,
-  ZINDEX_PREVIEW_PLACED_OBJECT,
-} from './zIndexes'
+  clearDrawingContainer,
+  createPixiAppStructure,
+  PADDING_CANVAS,
+  windowHeight,
+  windowWidth,
+} from './structure'
 
-const PADDING_CANVAS = 16
-const windowHeight = window.innerHeight
-const windowWidth = window.innerWidth
-const gridHeight = windowHeight - PADDING_CANVAS * 2
-const gridWidth = windowWidth - PADDING_CANVAS
+const { rootContainer, previewLayer, drawingContainer, eventsLayer } =
+  createPixiAppStructure()
 
 /**
  * This is where the magic happens.
@@ -32,56 +32,7 @@ export const Editor = () => {
   return <div id="editor-content"></div>
 }
 
-const useInitializePixiMainContainer = () => {
-  const { placeBlock, map, shouldRedraw, redrawIsFinished } = useMapContext()
-  const editorContainer = document.getElementById('editor-content')
-  const [, forceUpdate] = useReducer((x) => x + 1, 0)
-
-  useEffect(() => {
-    // only initialize the PIXI app once the div has been placed in the DOM
-    if (editorContainer == null) {
-      forceUpdate()
-      return
-    }
-
-    // avoid redrawing the PIXI app over and over due to variables updating
-    // this variable updates in very specific cases, such as importing a map
-    if (!shouldRedraw) {
-      return
-    }
-
-    const waitForInitializationPixiApp = async () => {
-      await initializePixiApp(editorContainer, {
-        placeBlock,
-        map,
-        shouldRedraw,
-      })
-    }
-
-    redrawIsFinished()
-
-    waitForInitializationPixiApp().catch(console.error)
-  }, [editorContainer, shouldRedraw, redrawIsFinished, placeBlock, map])
-
-  return editorContainer
-}
-
-type PixiEditorMapContext = Pick<
-  MapContextInformation,
-  'map' | 'placeBlock' | 'shouldRedraw'
->
-
-const initializePixiApp = async (
-  container: HTMLElement | null,
-  { placeBlock, map, shouldRedraw }: PixiEditorMapContext,
-) => {
-  if (container == null || !shouldRedraw) {
-    return
-  }
-  if (shouldRedraw) {
-    container.innerHTML = ''
-  }
-
+async function initializePixiApp(container: HTMLElement) {
   const app = new PIXI.Application()
 
   await app.init({
@@ -91,117 +42,142 @@ const initializePixiApp = async (
   })
   container.appendChild(app.canvas as unknown as Node)
 
-  const mainContainer = new PIXI.Container()
-  mainContainer.sortableChildren = true
-  mainContainer.renderable = true
+  return app
+}
 
-  const grid = generateGrid(gridWidth, gridHeight)
-  grid.x = PADDING_CANVAS
-  // grid.y = PADDING_CANVAS
-  const blocks = blocksLayer({
-    width: grid.width,
-    height: grid.height,
-    placeBlock,
+const useInitializePixiMainContainer = () => {
+  const mapContext = useMapContext()
+  const editorContext = useEditorContext()
+  const editorContainer = document.getElementById('editor-content')
+  const [, forceUpdate] = useReducer((x) => x + 1, 0)
+
+  const [pixiApp, setPixiApp] = useState<PIXI.Application | null>(null)
+
+  // init the app once and saves it in as a state
+  useEffect(() => {
+    if (pixiApp !== null) {
+      return () => {
+        pixiApp.destroy()
+      }
+    }
+    // DOM has not been loaded yet
+    if (editorContainer == null) {
+      forceUpdate()
+      return
+    }
+
+    // initialize the app
+    const waitForPixiAppInitialization = async () => {
+      const _pixiApp = await initializePixiApp(editorContainer)
+      _pixiApp.stage.addChild(rootContainer)
+
+      setPixiApp(_pixiApp)
+    }
+
+    waitForPixiAppInitialization().catch(console.error)
+  }, [pixiApp, editorContainer])
+
+  // after app is set, (re)draw the map
+  useEffect(() => {
+    if (pixiApp === null || !mapContext.shouldRedraw) {
+      return
+    }
+
+    drawMap({ map: mapContext.map })
+
+    mapContext.redrawIsFinished()
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- no need to redraw if redraw is not set
+  }, [pixiApp, mapContext.shouldRedraw])
+
+  // after app is set, (re)set event listeners for mousemove, pointerdown, ...
+  useEffect(() => {
+    if (pixiApp === null) {
+      return
+    }
+
+    resetEventsListeners({ editor: editorContext, map: mapContext })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pixiApp, editorContext.selectedObjectType])
+
+  return editorContainer
+}
+
+type DrawMapProps = {
+  map: MapInformation
+}
+function drawMap({ map }: DrawMapProps) {
+  clearDrawingContainer()
+
+  drawBlocksLayer({
     blocks: map.blocks,
   })
-  blocks.x = PADDING_CANVAS
-  // blocks.y = PADDING_CANVAS
-
-  mainContainer.addChild(grid)
-  mainContainer.addChild(blocks)
-
-  app.stage.addChild(mainContainer)
 }
 
 type BlocksLayerFuncParam = {
-  width: number
-  height: number
-  placeBlock: (b: Block) => void
   blocks: Block[]
 }
-function blocksLayer({
-  width,
-  height,
-  placeBlock,
-  blocks,
-}: BlocksLayerFuncParam) {
-  const layer = new PIXI.Container({ width, height })
-  layer.sortableChildren = true
+function drawBlocksLayer({ blocks }: BlocksLayerFuncParam) {
+  const blocksLayer = new PIXI.Graphics()
+  drawingContainer.addChild(blocksLayer)
 
-  // display a preview on current coordinates of the current object
-  const previewLayer = new PIXI.Container({
-    width,
-    height,
-    zIndex: ZINDEX_PREVIEW_PLACED_OBJECT,
+  // place blocks from the map
+  blocks.forEach((block) => {
+    blocksLayer.rect(block.x, block.y, CELL_SIZE, CELL_SIZE).fill(0x000000)
   })
-  layer.addChild(previewLayer)
+}
 
-  // where blocks are drawn
-  const blockGraphics = new PIXI.Graphics({ zIndex: ZINDEX_LAYER_BLOCKS })
+function resetEventsListeners({
+  editor,
+  map,
+}: {
+  editor: EditorContextInformation
+  map: MapContextInformation
+}) {
+  eventsLayer.removeAllListeners()
 
-  // this graphics is simply there to detect where the blocks will be drawn
-  const events = new PIXI.Graphics({ width, height, alpha: 0 })
-  events.zIndex = ZINDEX_CONTAINER_DETECT_EVENTS
-  events.eventMode = 'static'
-
-  events.on(
+  eventsLayer.on(
     'mousemove',
     onMouseMove(
       previewLayer,
       (props) => {
         switch (props.id) {
           case 'block': {
-            console.log({ props })
+            // console.log({ props })
             break
           }
           case 'spawn': {
-            console.log({ props })
+            // console.log({ props })
             break
           }
         }
       },
-      { id: 'block', x: 0, y: 0, type: 'lava', ambience: '' },
+      { id: editor.selectedObjectType, x: 0, y: 0, type: 'lava', ambience: '' },
     ),
   )
 
-  events.on(
+  eventsLayer.on(
     'pointerdown',
     onPointerDown(
-      layer,
+      drawingContainer,
       (properties: ObjProperties) => {
         switch (properties.id) {
           case 'block': {
-            placeBlock({ x: properties.x, y: properties.y })
+            map.placeBlock({ x: properties.x, y: properties.y })
             break
           }
           case 'spawn': {
-            console.log({ properties })
+            // console.log({ properties })
             break
           }
         }
       },
       {
-        id: 'spawn',
+        id: editor.selectedObjectType,
         type: 'lava',
-        // ambience: 'rainy',
+        ambience: 'rainy',
         x: 0,
         y: 0,
       },
     ),
   )
-
-  // place blocks from the map
-  blocks.forEach((block) => {
-    blockGraphics.rect(block.x, block.y, CELL_SIZE, CELL_SIZE).fill(0x000000)
-  })
-
-  layer.addChild(blockGraphics)
-
-  // define a rectangle so something appears and can be clicked
-  // even though it is transparent
-  events.rect(0, 0, width, height).fill(0xff00f0)
-
-  layer.addChild(events)
-
-  return layer
 }
