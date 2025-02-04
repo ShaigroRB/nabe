a8ec5563dc83848d069df48a4b88cb0ed98b3846
Shaigro <robin.boucher@epita.fr>
2025-02-05 00:55:36 +0100

    add colors by block types

--- src/colors.ts
+++ src/colors.ts
@@ -1,3 +1,14 @@
 export const COLOR_BLACK = 0x000000
 export const COLOR_PINK = 0xff00ff
 export const COLOR_RED = 0xff0000
+
+// Colors name based on what they represent in the game (poison, burn, heal, ...)
+export const COLOR_TYPE_POISON = 0xdd7df8
+export const COLOR_TYPE_NOWEAP = 0x000080
+export const COLOR_TYPE_ICY = 0x008080
+export const COLOR_TYPE_HEAL = 0x00ff00
+export const COLOR_TYPE_DMG = 0xffa040
+export const COLOR_TYPE_DEATH = 0xff0000
+export const COLOR_TYPE_BURN = 0xff6200
+export const COLOR_TYPE_BOUNCY = 0xff00ff
+export const COLOR_TYPE_AMMO = 0xffff00
